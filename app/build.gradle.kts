aca9c1fa746d450d868386cbd061f7d38439af90
Valllent <valllentua9128@gmail.com>
2024-05-22 21:35:29 +0200

    Integrate GAM, Prebid and Amazon ads.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,27 @@ android {
 }
 
 dependencies {
-
+    // Default libraries
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Image downloader
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+
+
+    // GAM and AdMob ads
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
+
+    // Amazon ads
+    implementation("com.amazon.android:aps-sdk:9.8.10")
+
+    // Prebid ads
+    implementation("org.prebid:prebid-mobile-sdk:2.2.1")
+
+
+    // Test libraries
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
